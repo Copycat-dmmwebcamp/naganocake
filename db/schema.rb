1c9c5f3eb7ddf74413f6a996b55dad20087a3eb2
vagrant <vagrant@localhost.localdomain>
2020-05-15 07:12:58 +0000

    add login-function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_113121) do
+ActiveRecord::Schema.define(version: 2020_05_15_053429) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,10 +37,9 @@ ActiveRecord::Schema.define(version: 2020_05_14_113121) do
     t.string "postal_code"
     t.string "address"
     t.string "phone_number"
-    t.string "password"
-    t.boolean "user_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "user_status", default: true, null: false
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
