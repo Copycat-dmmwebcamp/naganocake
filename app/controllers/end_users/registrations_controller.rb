6d6c2a2a9550bacb3f18ada9a366ea2565f1e4f4
vagrant <vagrant@localhost.localdomain>
2020-05-16 12:25:30 +0000

    fix bugs

--- app/controllers/end_users/registrations_controller.rb
+++ app/controllers/end_users/registrations_controller.rb
@@ -39,8 +39,7 @@ class EndUsers::RegistrationsController < Devise::RegistrationsController
   # end
 
   # protected
-
-
+  
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
