3f11a1fb56ccb2d79049f2887f6c258a0b81fd96
vagrant <vagrant@localhost.localdomain>
2020-05-17 12:50:41 +0000

    add end_user_items

--- app/controllers/end_users/items_controller.rb
+++ app/controllers/end_users/items_controller.rb
@@ -1,4 +1,32 @@
 class EndUsers::ItemsController < ApplicationController
-  def index
+
+  def top
+  	# おすすめ商品かつ販売中のもの
+  	@items = Item.where(recomend: true, item_status: 0)
+  	@categories = Category.all
+
   end
+
+  def index
+  	@items = Item.all
+  	@categories = Category.all
+  	@id = 0
+
+  end
+
+  def show
+  	@item = Item.find(params[:id])
+  	@categories = Category.all
+  end
+
+  def search
+  	@categories = Category.all
+  	@id = params[:category_id]
+  	@category = Category.find(@id)
+  	# カテゴリーと一致する商品を探す
+  	@items = Item.where(category_id: @id)
+  	render 'index'
+
+  end
+
 end
