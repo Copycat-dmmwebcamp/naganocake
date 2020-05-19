611f90c50e94f55df0644a30642fa1a4e7529552
vagrant <vagrant@localhost.localdomain>
2020-05-19 07:35:17 +0000

    fix cart

--- app/controllers/end_users/orders_controller.rb
+++ app/controllers/end_users/orders_controller.rb
@@ -26,17 +26,49 @@ class EndUsers::OrdersController < ApplicationController
   	else
   		# 新たに入力した住所を選択
   	end
+    @cart_items = current_end_user.cart_items
   	render 'index'
   end
 
   def index
-  	@order = Order.new
+
+  end
+
+# 注文と注文商品の作成
+  def create
+    @order = Order.new(order_params)
+    @order.end_user_id = current_end_user.id
+    @order.save
+    # カート内の商品をを注文商品に入れる
+    @cart_items = current_end_user.cart_items
+     @cart_items.each do |cart_item|
+      @order_item = OrderItem.new
+      # 商品の特定
+      item = Item.find(cart_item.item_id)
+      @order_item.order_id = @order.id
+      @order_item.item_id = item.id
+      @order_item.production_status = "status0"
+      @order_item.unit_price = (item.without_tax.to_i * 1.1).round
+      @order_item.item_number = cart_item.item_number
+      # 注文商品のセーブ
+       @order_item.save
+     end
+     # カート内アイテムの全消去
+     cart_items = CartItem.where(params[user_id: current_end_user.id])
+     cart_items.each do |cart_item|
+     cart_item.destroy
+     end
+     redirect_to end_users_orders_complete_path
+  end
+
+  def complete
+
   end
 
   private
 
   def order_params
   	# new画面から受け取るパラメーター
-  	params.require(:order).permit(:payment_info, :destination, :delivery_address, :postal_code, :postage, :billing_amount)
+  	params.require(:order).permit(:payment_info, :destination, :delivery_address, :postal_code, :postage, :billing_amount, :order_status)
   end
 end
