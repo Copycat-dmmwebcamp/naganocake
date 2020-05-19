48988784a2d6d70962b201938a8468255c9da432
yusaku0515 <y.iwata0515@gmail.com>
2020-05-19 07:36:32 +0000

    no add

--- app/controllers/admins/order_histories_controller.rb
+++ app/controllers/admins/order_histories_controller.rb
@@ -3,6 +3,13 @@ class Admins::OrderHistoriesController < ApplicationController
   # このコントローラが動作する前に実行、admin側でログイン認証できていなければadmin側のログイン画面へ
   before_action :authenticate_admin!
 
+  def index
+  	@orders = Order.all
+  end
+
+  def show
+  end
+
   def update_order
   end
 
