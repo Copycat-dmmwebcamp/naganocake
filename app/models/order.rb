90b62ad5282451c429750d53c02a0eadb7c93921
kane00 <nkaneko00@gmail.com>
2020-05-20 06:28:16 +0000

    add validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -9,6 +9,14 @@ class Order < ApplicationRecord
     # credit = クレジット　bank = 銀行払い
 
 
-=======
+    validates :payment_info, inclusion: { in: ["0", "1"] }
+    validates :destination, presence: true
+    validates :delivery_address, presence: true
+    validates :postal_code, presence: true
+    validates :postage, presence: true
+    validates :billing_amount, presence: true
+    # enum型、数値に変わっているため０〜４が含まれているかを書く
+    validates :order_status, inclusion: {in: [ "0", "1", "2", "3", "4"] }
+    validates :end_user_id, presence: true
 
 end
