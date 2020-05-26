793a29a553f00ffa25ecec5209e565ab18c7e23f
vagrant <vagrant@localhost.localdomain>
2020-05-26 07:25:05 +0000

    adjust SCSS

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd28126ba70ca08f560109c8b3770bc4c9a71a78daa59bf5c4b2b040d24e92d2d7c42224f1687f9334a29aeb3ac86d226981baa3a13ad35aeda974f7bb63b3cd9'
